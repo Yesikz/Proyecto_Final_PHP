99c568adb13428072dfd39dd585e00baeb91620b
Yesica Fica Millan <ficamillan@gmail.com>
2024-06-14 10:57:11 -0300

    [v1] Construccion pagina Registro Peliculas

--- js/registro.js
+++ js/registro.js
@@ -25,19 +25,29 @@ var btn = document.getElementById("termsLink");
 // Obtener el elemento <span> que cierra el modal
 var span = document.getElementsByClassName("close")[0];
 
-// Cuando el usuario haga clic en el botón, abre el modal
-btn.onclick = function (event) {
-    event.preventDefault(); // Evitar el comportamiento por defecto del enlace
+// Función para abrir el modal
+function openModal() {
     modal.style.display = "block";
 }
 
+// Cuando el usuario haga clic en el botón, abre el modal
+btn.onclick = function(event) {
+    event.preventDefault(); // Evitar el comportamiento por defecto del enlace
+    openModal();
+}
+
+// Cuando el usuario haga clic en el li del footer, abre el modal
+document.getElementById("footerTerminos").onclick = function() {
+    openModal();
+}
+
 // Cuando el usuario haga clic en <span> (x), cierra el modal
-span.onclick = function () {
+span.onclick = function() {
     modal.style.display = "none";
 }
 
 // Cuando el usuario haga clic en cualquier parte fuera del modal, lo cierra
-window.onclick = function (event) {
+window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
